03723cb3c920acff4ae58ecba33363764a421a43
Ethan Philipson <erphilipson@gmail.com>
2017-06-26 10:02:41 -0400

    Rename unemployed job variable

--- index.js
+++ index.js
@@ -10,7 +10,20 @@ app.set('views', './views')
 app.set('view engine', 'mustache')
 
 app.get('/', function(req, res) {
-  res.render('index', {data})
+  res.render('index', {users: data.users})
+})
+
+app.get('/:id', function(req, res) {
+  let id = req.params.id;
+  let name = data.users[id-1].name;
+  let avatar = data.users[id-1].avatar;
+  let address = data.users[id-1].address;
+  let job = data.users[id-1].job;
+  let email = data.users[id-1].email;
+  let company = data.users[id-1].company;
+  let phone = data.users[id-1].phone;
+
+  res.render('test', {name, email, address})
 })
 
 app.listen(3000, function () {
